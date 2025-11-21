16150a7303f3a316ff870006d24029dc976d1e89
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-11-21 20:15:56 +0530

    permution

---
+++ permation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(vector<int>& nums, int index) {
+      
+        if(index == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        
+        for(int i = index; i < nums.size(); i++) {
+            swap(nums[i], nums[index]);    
+            backtrack(nums, index + 1);    
+            swap(nums[i], nums[index]);   
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        backtrack(nums, 0);
+        return ans;
+    }
+};
